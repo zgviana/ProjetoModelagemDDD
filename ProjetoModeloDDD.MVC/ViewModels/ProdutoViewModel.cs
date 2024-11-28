f311c3c9b32445377a9e2335cc57d8b58f4a2b3e
giovanni.viana <zgviana@gmail.com>
2024-11-27 22:45:03 -0300

    Alterações no controller do produto

--- ProjetoModeloDDD.MVC/ViewModels/ProdutoViewModel.cs
+++ ProjetoModeloDDD.MVC/ViewModels/ProdutoViewModel.cs
@@ -9,7 +9,7 @@ namespace ProjetoModeloDDD.MVC.ViewModels
         [Key]
         public int ProdutoId { get; set; }
 
-        [Required(ErrorMessage = "Preencha o campo Giovanni")]
+        [Required(ErrorMessage = "Preencha o campo Nome")]
         [MaxLength(250, ErrorMessage = "Máximo {0} caracteres")]
         [MinLength(2, ErrorMessage = "Mínimo {0} caracteres")]
         public string Nome { get; set; }
